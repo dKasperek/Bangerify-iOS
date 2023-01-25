2c84ebdab0511f84956e6f2c78a8259abcbc71e9
dKasperek <david308dk@icloud.com>
2023-01-25 19:18:10 +0100

    Adding EnvironmentObject to ContentView.swift

--- Bangerify/ContentView.swift
+++ Bangerify/ContentView.swift
@@ -8,15 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var network: Network
     var body: some View {
         NavigationView {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
+            List {
                 Text("Hello, world!")
             }
-            .padding()
             .navigationTitle("Bangerify")
         }
         
@@ -26,5 +23,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(Network())
     }
 }

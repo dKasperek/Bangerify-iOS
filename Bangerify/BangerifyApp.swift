0fccfd9490b4f75aace3b4c50011b20519296e78
dKasperek <david308dk@icloud.com>
2023-01-25 18:31:27 +0100

    Initial Commit

---
+++ Bangerify/BangerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BangerifyApp.swift
+//  Bangerify
+//
+//  Created by David Kasperek on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BangerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

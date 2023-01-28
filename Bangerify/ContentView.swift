ec3ca9b5c1afc9c59e1d34e6d2b25c0b2b4272f0
dKasperek <david308dk@icloud.com>
2023-01-28 16:49:04 +0100

    Post code moved to file PostUI

--- Bangerify/ContentView.swift
+++ Bangerify/ContentView.swift
@@ -13,51 +13,9 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(network.posts) { post in
-                VStack {
-                    HStack {
-                        AsyncImage(url: URL(string: post.profilePictureUrl)) { image in image
-                                .resizable()
-                        } placeholder: {
-                            Color.gray
-                        }
-                            .clipShape(Circle())
-                            .frame(width: 50, height: 50)
-                        VStack (alignment: .leading){
-                            HStack {
-                                Text(post.visibleName)
-                                    .font(.headline)
-                                Spacer()
-                                Image(systemName: "ellipsis")
-                                    .font(Font.system(.caption))
-                                    .foregroundColor(.secondary)
-                            }
-                            HStack {
-                                Text("@" + post.username).font(.caption)
-                                Spacer()
-                                Text(post.date)                    .font(.caption)
-                                    .fontWeight(.light)
-                                    .foregroundColor(.secondary)
-                                
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                    } .padding(5)
-                    
-                    Text(post.text)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    HStack {
-                        Image(systemName: "heart")
-                            .font(Font.system(.title3))
-                        Text(String(network.getLikeCount(postId: post.id)))
-                        Spacer()
-                        Image(systemName: "bubble.left")
-                            .font(Font.system(.title3))
-                        Text("0").font(Font.system(.title3))
-                    } .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(5)
-                }
-                
+                PostView(post: post)
             }
+            
             .navigationTitle("Bangerify")
             .onAppear{
                 network.getPosts()

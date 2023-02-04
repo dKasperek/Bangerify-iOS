9e900819f55134f7a017943681c5328ab6140307
dKasperek <david308dk@icloud.com>
2023-02-04 12:51:54 +0100

    Fixed MarkdownHelper.divideToArray error, that nothing was returned when a post doesn't contain an image

--- Bangerify/MarkdownHelper.swift
+++ Bangerify/MarkdownHelper.swift
@@ -8,24 +8,29 @@
 import Foundation
 
 public func divideToArray(rawString: String) -> [String] {
-    let componentsOfString = rawString.split(separator: "![")
     var markupList: [String] = []
+    
+    if rawString.contains("!["){
+        let componentsOfString = rawString.split(separator: "![")
 
-    for (index, element) in componentsOfString.enumerated() {
-        if index == 0 && componentsOfString.count > 1 {
-            markupList.append(String(element))
-        }
-        else {
-            let newElement = "![" + element
-            if let index = newElement.firstIndex(of: ")") {
-                markupList.append(String(newElement[..<index]))
-                markupList.append(String(newElement[newElement.index(after: index)...]))
+        for (index, element) in componentsOfString.enumerated() {
+            if index == 0 && componentsOfString.count > 1 {
+                markupList.append(String(element))
+            }
+            else {
+                let newElement = "![" + element
+                if let index = newElement.firstIndex(of: ")") {
+                    markupList.append(String(newElement[..<index]))
+                    markupList.append(String(newElement[newElement.index(after: index)...]))
+                }
             }
         }
-    }
 
-    markupList = markupList.filter { (element: String) -> Bool in
-        return !element.isEmpty }
+        markupList = markupList.filter { (element: String) -> Bool in
+            return !element.isEmpty }
+    } else {
+        markupList.append(rawString)
+    }
     
     return markupList
 }

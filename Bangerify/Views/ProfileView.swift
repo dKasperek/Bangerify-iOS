edccbeaafe5142b441ffb203870004b32ce9b651
dKasperek <david308dk@icloud.com>
2023-04-02 22:55:48 +0200

    isOwner check in ProfileView, ProfileHeaderComponent merged back to ProfileView

--- Bangerify/Views/ProfileView.swift
+++ Bangerify/Views/ProfileView.swift
@@ -14,15 +14,56 @@ import Network
 struct ProfileView: View {
     let username: String
     
+    var isOwner: Bool
     @State private var profile: Profile?
     @State private var posts: [Post]?
     
+    init(username: String) {
+        self.username = username
+        self.isOwner = (self.username == AuthenticationService.shared.getUsername())
+    }
+
     
     var body: some View {
         NavigationView {
             if let profile = profile, let posts = posts {
                 ScrollView {
-                    ProfileHeaderComponent(profile: profile, username: username)
+                    HStack {
+                        KFImage(URL(string: profile.profilePictureUrl ?? ""))
+                            .placeholder {
+                                Image(systemName: "hourglass")
+                                    .foregroundColor(.gray)
+                            }
+                            .cancelOnDisappear(true)
+                            .resizable()
+                            .clipShape(Circle())
+                            .frame(width: 75, height: 75)
+                        
+                        VStack (alignment: .leading){
+                            HStack {
+                                Text(profile.visibleName)
+                                    .font(.title2)
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(getGradeColor(grade: profile.grade))
+                                Spacer()
+                                if isOwner {
+                                    Image(systemName: "ellipsis")
+                                        .font(Font.system(.body))
+                                        .foregroundColor(.secondary)
+                                }
+                            }
+                            HStack {
+                                Text("@" + username)
+                                    .font(.headline)
+                                    .fontWeight(.light)
+                                
+                            }.frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    } .padding(5)
+                    
+                    Markdown(profile.bio ?? "")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(10)
                     
                     LazyVStack {
                         ForEach(posts, id: \.id) { post in

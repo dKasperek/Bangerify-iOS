f9bca8db9a3c24a828487f48aaa3dac5effadbf7
dKasperek <david308dk@icloud.com>
2023-04-03 22:55:10 +0200

    ProfileService loadProfile rework with Alamofire

--- Bangerify/Services/ProfileService.swift
+++ Bangerify/Services/ProfileService.swift
@@ -6,38 +6,24 @@
 //
 
 import Foundation
+import Alamofire
 
 class ProfileService {
     
     static let shared = ProfileService()
     
     func loadProfile(username: String, completion: @escaping (Profile?) -> Void) {
-        let profileUrl = "http://3.71.193.242:8080/api/userData/" + username
-        guard let url = URL(string: profileUrl) else { fatalError("Missing URL") }
+        let profileUrl = "http://3.71.193.242:8080/api/userData/\(username)"
         
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "POST"
-        
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
+        AF.request(profileUrl, method: .post).responseDecodable(of: [Profile].self) { response in
+            switch response.result {
+            case .success(let profiles):
+                completion(profiles.first)
+            case .failure(let error):
                 print("Request error: ", error)
-                return
-            }
-            
-            guard let data = data else {
-                print("Invalid data from request: ", url)
-                return
-            }
-            
-            do {
-                let profileArray = try JSONDecoder().decode([Profile].self, from: data)
-                let profile = profileArray.first!
-                completion(profile)
-            } catch let error {
-                print("Error decoding: ", error)
+                completion(nil)
             }
         }
-        dataTask.resume()
     }
     
 }
--- Bangerify/Views/ProfileView.swift
+++ Bangerify/Views/ProfileView.swift
@@ -28,63 +28,66 @@ struct ProfileView: View {
         NavigationView {
             if let profile = profile, let posts = posts {
                 ScrollView {
-                    HStack {
-                        KFImage(URL(string: profile.profilePictureUrl ?? ""))
-                            .placeholder {
-                                Image(systemName: "person.circle")
-                                    .resizable()
-                                    .foregroundColor(.gray)
-                                    .frame(width: 75, height: 75)
-                            }
-                            .cancelOnDisappear(true)
-                            .resizable()
-                            .clipShape(Circle())
-                            .frame(width: 75, height: 75)
+                    VStack {
+                        Spacer().frame(height: 20)
+                        HStack {
+                            KFImage(URL(string: profile.profilePictureUrl ?? ""))
+                                .placeholder {
+                                    Image(systemName: "person.circle")
+                                        .resizable()
+                                        .foregroundColor(.gray)
+                                        .frame(width: 75, height: 75)
+                                }
+                                .cancelOnDisappear(true)
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 75, height: 75)
 
-                        
-                        VStack (alignment: .leading){
-                            HStack {
-                                Text(profile.visibleName)
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(getGradeColor(grade: profile.grade))
-                                Spacer()
-                                if isOwner {
-                                    Menu {
-                                        Button(action: {
-                                            // Edut visible name
-                                        }) {
-                                            Label("Edit visible name", systemImage: "pencil.line")
+                            
+                            VStack (alignment: .leading){
+                                HStack {
+                                    Text(profile.visibleName)
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(getGradeColor(grade: profile.grade))
+                                    Spacer()
+                                    if isOwner {
+                                        Menu {
+                                            Button(action: {
+                                                // Edut visible name
+                                            }) {
+                                                Label("Edit visible name", systemImage: "pencil.line")
+                                            }
+                                            
+                                            Button(action: {
+                                                // Edit profile picture
+                                            }) {
+                                                Label("Change profile picture", systemImage: "camera")
+                                            }
+                                            
+                                            Button(action: {
+                                                // Edit bio text
+                                            }) {
+                                                Label("Change BIO", systemImage: "square.and.pencil")
+                                            }
+                                            
+                                            
+                                        } label: {
+                                            Image(systemName: "ellipsis.circle")
+                                                .font(Font.system(.body))
+                                                .foregroundColor(.primary)
                                         }
-                                        
-                                        Button(action: {
-                                            // Edit profile picture
-                                        }) {
-                                            Label("Change profile picture", systemImage: "camera")
-                                        }
-                                        
-                                        Button(action: {
-                                            // Edit bio text
-                                        }) {
-                                            Label("Change BIO", systemImage: "square.and.pencil")
-                                        }
-                                        
-                                        
-                                    } label: {
-                                        Image(systemName: "ellipsis.circle")
-                                            .font(Font.system(.body))
-                                            .foregroundColor(.primary)
                                     }
                                 }
+                                HStack {
+                                    Text("@" + username)
+                                        .font(.headline)
+                                        .fontWeight(.light)
+                                    
+                                }.frame(maxWidth: .infinity, alignment: .leading)
                             }
-                            HStack {
-                                Text("@" + username)
-                                    .font(.headline)
-                                    .fontWeight(.light)
-                                
-                            }.frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                    } .padding(5)
+                        } .padding(5)
+                    }
                     
                     Markdown(profile.bio ?? "")
                         .frame(maxWidth: .infinity, alignment: .leading)

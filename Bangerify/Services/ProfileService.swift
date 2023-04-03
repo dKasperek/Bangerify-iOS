cf7ad6ab4efa7ce34b156fdc26e0d45792dd7354
dKasperek <david308dk@icloud.com>
2023-04-03 23:09:35 +0200

    ChangeVisibleName and ChangeBio functions in ProfileService

--- Bangerify/Services/ProfileService.swift
+++ Bangerify/Services/ProfileService.swift
@@ -11,6 +11,7 @@ import Alamofire
 class ProfileService {
     
     static let shared = ProfileService()
+    let authenticationService = AuthenticationService.shared
     
     func loadProfile(username: String, completion: @escaping (Profile?) -> Void) {
         let profileUrl = "http://3.71.193.242:8080/api/userData/\(username)"
@@ -26,4 +27,85 @@ class ProfileService {
         }
     }
     
+    func changeVisibleName(newVisibleName: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let url = URL(string: "http://3.71.193.242:8080/api/changeVisibleName") else { fatalError("Missing URL") }
+        
+        authenticationService.getValidAccessToken { accessToken in
+            guard let accessToken = accessToken else {
+                print("Error getting valid access token")
+                return
+            }
+            
+            let parameters: [String: Any] = [
+                "newVisibleName": newVisibleName
+            ]
+            
+            let headers: HTTPHeaders = [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(accessToken)"
+            ]
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "POST"
+            urlRequest.allHTTPHeaderFields = headers.dictionary
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+                urlRequest.httpBody = jsonData
+            } catch {
+                completion(.failure(error))
+                return
+            }
+            
+            AF.request(urlRequest).response { response in
+                switch response.result {
+                case .success:
+                    completion(.success(()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    func changeBio(newBio: String, completion: @escaping (Result<Void, Error>) -> Void) {
+        guard let url = URL(string: "http://3.71.193.242:8080/api/changeBio") else { fatalError("Missing URL") }
+        
+        authenticationService.getValidAccessToken { accessToken in
+            guard let accessToken = accessToken else {
+                print("Error getting valid access token")
+                return
+            }
+            
+            let parameters: [String: Any] = [
+                "newBio": newBio
+            ]
+            
+            let headers: HTTPHeaders = [
+                "Content-Type": "application/json",
+                "Authorization": "Bearer \(accessToken)"
+            ]
+            
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = "POST"
+            urlRequest.allHTTPHeaderFields = headers.dictionary
+            
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: parameters, options: [])
+                urlRequest.httpBody = jsonData
+            } catch {
+                completion(.failure(error))
+                return
+            }
+            
+            AF.request(urlRequest).response { response in
+                switch response.result {
+                case .success:
+                    completion(.success(()))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
 }
